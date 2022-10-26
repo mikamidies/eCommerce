c8d49747a62015d64c045ce1b293ce19ebbbac7e
mikamidies <mikamidies04@gmail.com>
2022-10-26 17:27:41 +0500

    First preporations

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'ECommerce',
     meta: [
@@ -19,57 +16,66 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/base.css', '@sweetalert2/theme-material-ui'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    'vue-sweetalert2/nuxt/no-css',
+    'nuxt-webfontloader',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-  ],
+  webfontloader: {
+    google: {
+      families: ['DM+Sans:wght@400;500;700&display=swap'],
+    },
+  },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa', '@nuxt/content'],
+
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: 'en',
     },
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/css/variables.scss'],
+    defaultAssets: {
+      font: {
+        family: 'DM Sans',
+        size: 15,
+      },
+    },
+    treeShake: true,
     theme: {
-      dark: true,
+      light: true,
       themes: {
+        light: {
+          primary: colors.deepPurple.darken1,
+          bg: '#ffffff',
+          surface: '#ffffff',
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
         dark: {
-          primary: colors.blue.darken2,
+          primary: colors.deepPurple.darken1,
+          bg: '#0a0514',
+          surface: '#130a24',
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
@@ -81,6 +87,5 @@ export default {
     },
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
